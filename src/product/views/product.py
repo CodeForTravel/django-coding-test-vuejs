399a4462e09305d301aaabeff94ab9c86f6aaa40
CodeForTravel <mdfaisalnstu@gmail.com>
2022-04-22 17:07:22 +0600

    product.py updated

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -4,11 +4,20 @@ from product.models import Variant
 
 
 class CreateProductView(generic.TemplateView):
-    template_name = 'products/create.html'
+    template_name = "products/create.html"
 
     def get_context_data(self, **kwargs):
         context = super(CreateProductView, self).get_context_data(**kwargs)
-        variants = Variant.objects.filter(active=True).values('id', 'title')
-        context['product'] = True
-        context['variants'] = list(variants.all())
+        variants = Variant.objects.filter(active=True).values("id", "title")
+        context["product"] = True
+        context["variants"] = list(variants.all())
+        return context
+
+
+class ProductListView(generic.TemplateView):
+    template_name = "products/product_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context["product"] = True
         return context
