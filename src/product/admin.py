08b7e37b4b0778a945affea353689716fdb922d8
CodeForTravel <mdfaisalnstu@gmail.com>
2022-04-22 17:05:11 +0600

    admin.py updated

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from product import models as models_product
 
-# Register your models here.
+admin.site.register(models_product.Variant)
+admin.site.register(models_product.Product)
+admin.site.register(models_product.ProductImage)
+admin.site.register(models_product.ProductVariant)
+admin.site.register(models_product.ProductVariantPrice)
