637a0ff0e705bf38c50545db3caf504a81782fa4
CodeForTravel <mdfaisalnstu@gmail.com>
2022-04-22 17:08:24 +0600

    api created

---
+++ src/product/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from product.api import views as views_product
+
+simple_router = routers.SimpleRouter(trailing_slash=True)
+simple_router.register(r"products", views_product.ProductViewSet, basename="product")
+
+urlpatterns = [
+    path("", include(simple_router.urls)),
+]
---
+++ src/product/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets, filters
+from product.api import filters as filters_product
+from product.api import pagination as pagination_global
+from product.api import serializers as serializers_product
+from product import models as models_product
+
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models_product.Product.objects.all().order_by("-created_at")
+    serializer_class = serializers_product.ProductSerializer
+    pagination_class = pagination_global.GlobalPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = [
+        "title",
+        "description",
+    ]
